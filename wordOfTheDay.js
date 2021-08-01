57ec02c8c3d892a22c0aa5591964b282ba4aa130
Chris Mo <cmochinski@gmail.com>
2021-08-01 13:34:28 -0500

    new fun fact - terminal file info

--- wordOfTheDay.js
+++ wordOfTheDay.js
@@ -1,17 +1,26 @@
 //THIS IS MY 'WORD OF THE DAY' FILE
 //I'll use this to keep track of terms I find interesting.
+//----------------------------------------------------
+        //WEEK-1------------
 
-        //WEEK-1
-//
     //CONCATENATE - link things together in a chain or series
 //"some words may be concatenated, such that certain sounds are omitted"
-//
 
-        //WEEK-2
-//
+//----------------------------------------------------
+
+
+        //WEEK-2------------
+
     //DELINEATE - describe or portray something precisely
 //"the law shoud delineate and prohibit behavior that is socially abhorrent"
 //OR - to indicate the exact position of (a border or boundary)
-//
+
+//----------------------------------------------------
+
+
+        //WEEK-3-------------
+
     //INSTANTIATE - represent as or by an instance
 //"a study of two groups who seemed to instantiate productive aspects of this"
+//
+    //CLI - "Command Line Interface" - should probably figure out if this term is common
