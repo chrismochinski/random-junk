cdd803d2cb4380d1d03e34ab1328379826666ffa
Chris Mo <cmochinski@gmail.com>
2021-08-13 14:04:09 -0500

    updated template literal

--- wordOfTheDay.js
+++ wordOfTheDay.js
@@ -3,38 +3,40 @@ console.log('some fun facts in the notes...');
 //THIS IS MY 'WORD OF THE DAY' FILE
 //I'll use this to keep track of terms I find interesting.
 //----------------------------------------------------
-        //WEEK-1------------
+        //WEEK-1------------------------
 
-    //CONCATENATE - link things together in a chain or series
+    //CONCATENATE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//link things together in a chain or series
 //"some words may be concatenated, such that certain sounds are omitted"
 
 //----------------------------------------------------
 
+        //WEEK-2------------------------
 
-        //WEEK-2------------
-
-    //DELINEATE - describe or portray something precisely
+    //DELINEATE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//describe or portray something precisely
 //"the law shoud delineate and prohibit behavior that is socially abhorrent"
 //OR - to indicate the exact position of (a border or boundary)
 
 //----------------------------------------------------
 
+        //WEEK-3-------------------------
 
-        //WEEK-3-------------
-
-    //INSTANTIATE - represent as or by an instance
+    //INSTANTIATE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//represent as or by an instance
 //"a study of two groups who seemed to instantiate productive aspects of this"
 //
-    //C.L.I. (no dots) - "Command Line Interface" - should probably figure out if this term is common
+    //C.L.I. (no dots)~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//"Command Line Interface" - should probably figure out if this term is common
 
     //----------------------------------------------------
 
+            //WEEK-5-------------------------
 
-            //WEEK-5-------------
+    // COMPOUND CONDITIONAL ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//uses OR which is || in the syntax
 
-    // COMPOUND CONDITIONAL - uses OR which is ||
-
-    //PARSE / PARSING -
+    //PARSE / PARSING ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 /* To parse is to break up a sentence or group of words into
 separate components, including the definition of each part's
 function or form. ... Parsing is used in all high-level
@@ -43,18 +45,88 @@ by their respective compilers before being transformed
 into executable machine code.
 */
 
-    //NaN -
-/* 'Not A Number'
-Used to denote that the value of an object is NOT a number
-*/
+    //NaN ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//'Not A Number'
+//Used to denote that the value of an object is NOT a number
 
-    //null -
+    //null ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //object is empty and isn't pointing to any memory address
 //exits but has been deliberately been given an empty value
 
-    //undefined
+    //undefined ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //The object doesn' thave any value, therefore undefined.
 //When you create a variable but don't assign a value to it.
 //Exists but hasn't been given a value...yet
 
-    //
+    //ARROW FUNCTION EXPRESSIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+/* These are NOT to be used yet
+This is a compact alternative to traditional function expressions,
+but is limited and cannot be used in all situations
+
+-Does not have its own bindings to this or super, and should not be used as methods.
+-Does not have new.target keyword.
+-Not suitable for call, apply and bind methods, which generally rely on establishing a scope.
+-Can not be used as constructors.
+-Can not use yield, within its body.
+
+example............
+
+// Traditional Function
+function (a){
+  return a + 100;
+}
+
+// Arrow Function Break Down
+
+// 1. Remove the word "function" and place arrow between the argument and opening body bracket
+(a) => {
+  return a + 100;
+}
+
+// 2. Remove the body braces and word "return" -- the return is implied.
+(a) => a + 100;
+
+// 3. Remove the argument parentheses
+a => a + 100;
+Copy to Clipboard
+
+----------------------
+----------------------
+*/
+
+    //Boilerplate code ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+/*boilerplate code or just boilerplate are sections of code
+that are repeated in multiple places with little to no variation.
+When using languages that are considered verbose, the programmer
+must write a lot of code to accomplish only minor functionality.
+Such code is called boilerplate
+
+The term arose from the newspaper business. Columns and other pieces
+that were distributed by print syndicates were sent to subscribing
+newspapers in the form of prepared printing plates. Because of their
+resemblance to the metal plates used in the making of boilers, they
+became known as "boiler plates", and their resulting text -
+"boilerplate text". As the stories that were distributed by boiler
+plates were usually "fillers" rather than "serious" news, the term
+became synonymous with unoriginal, repeated text.
+*/
+
+    //TEMPLATE LITERALS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+/*String literals allowing embedded expressions.
+You can use multi-line strings and interpolation features
+Enclose in backtick and curly braces (${expression})
+
+`string text`
+
+`string text line 1
+ string text line 2`
+
+`string text ${expression} string text`
+
+tag`string text ${expression} string text`
+
+To escape a backtick in a template literal, put a backslash (\) before the backtick.
+
+`\`` === '`' // --> true
+
+*/
